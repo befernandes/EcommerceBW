feb3248c248bce2fc3b716382e9fafdbd691305f
befernandes <benildomingos@hotmail.com>
2025-10-14 20:54:23 -0400

    Start project

---
+++ src/main/java/com/ecombw/EcommerceBW/EcommerceBwApplication.java
@@ -0,0 +1,13 @@
+package com.ecombw.EcommerceBW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBwApplication.class, args);
+	}
+
+}
