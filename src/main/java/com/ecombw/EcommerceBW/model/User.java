a2254c98167843a9b5879c25c6cf270ebdd7392b
befernandes <benildomingos@hotmail.com>
2025-10-22 18:32:56 -0400

    Update Entity User

--- src/main/java/com/ecombw/EcommerceBW/model/User.java
+++ src/main/java/com/ecombw/EcommerceBW/model/User.java
@@ -1,15 +1,13 @@
 package com.ecombw.EcommerceBW.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
 
 @Entity
+@Table(name = "users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
