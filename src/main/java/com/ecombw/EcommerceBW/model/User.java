775b37546ddf4c5d96668cbf4312bacce98dd021
befernandes <benildomingos@hotmail.com>
2025-10-22 18:25:35 -0400

    Create Entity User

---
+++ src/main/java/com/ecombw/EcommerceBW/model/User.java
@@ -0,0 +1,27 @@
+package com.ecombw.EcommerceBW.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
+    @NonNull
+    private String email;
+    private String roles;
+}
